6e9beaca4048567e26f64c14eae7000be79e8159
whosoeverHoldsThisHammer <andresjfiorito@hotmail.com>
2025-08-09 14:49:19 -0300

    Se agrega estrcutura de datos para el proyecto

---
+++ src/model/tournament.js
@@ -0,0 +1,525 @@
+const results = {
+    roundsixteen: [
+        {
+            id: "",
+            stage: "Octavos de final",
+            coupleA: {
+                players: [
+                    {
+                        name: "Paula Josemaría",
+                        age: 28,
+                        country: "Spain"
+                    },
+                    {
+                        name: "Ariana Sánchez",
+                        age: 28,
+                        country: "Spain"
+                    }
+                ],
+                score: [7, 6]
+            },
+            coupleB: {
+                players: [
+                    {
+                        name: "Araceli Martinez",
+                        age: 24,
+                        country: "Spain"
+                    },
+                    {
+                        name: "Allix Colombon",
+                        age: 32,
+                        country: "France"
+                    }
+                ],
+                score: [6, 4]
+            },
+        },
+        {
+            id: "",
+            stage: "Octavos de final",
+            coupleA: {
+                players: [
+                    {
+                        name: "Jessica Castello",
+                        age: 26,
+                        country: "Spain"
+                    },
+                    {
+                        name: "Lorena Rufo Ortiz",
+                        age: 22,
+                        country: "Spain"
+                    }
+                ],
+                score: [6, 6]
+            },
+            coupleB: {
+                players: [
+                    {
+                        name: "Alejandra Salazar",
+                        age: 39,
+                        country: "Spain"
+                    },
+                    {
+                        name: "Martina Calvo Santamarina",
+                        age: 17,
+                        country: "France"
+                    }
+                ],
+                score: [2, 2]
+            },
+        },
+        {
+            id: "",
+            stage: "Octavos de final",
+            coupleA: {
+                players: [
+                    {
+                        name: "Aranzazu Osoro",
+                        age: 27,
+                        country: "Argentina"
+                    },
+                    {
+                        name: "Verónica Virseda",
+                        age: 33,
+                        country: "Spain"
+                    }
+                ],
+                score: [5, 6, 1]
+            },
+            coupleB: {
+                players: [
+                    {
+                        name: "Marina Guinart España",
+                        age: 29,
+                        country: "Spain"
+                    },
+                    {
+                        name: "Victoria Iglesias",
+                        age: 31,
+                        country: "Spain"
+                    }
+                ],
+                score: [7, 4, 6]
+            },
+        },
+        {
+            id: "",
+            stage: "Octavos de final",
+            coupleA: {
+                players: [
+                    {
+                        name: "Noemi Aguilar Carrillo",
+                        age: 18,
+                        country: "Spain"
+                    },
+                    {
+                        name: "Julia Polo Bautista",
+                        age: 26,
+                        country: "Spain"
+                    }
+                ],
+                score: [2, 4]
+            },
+            coupleB: {
+                players: [
+                    {
+                        name: "Sofía Arújo",
+                        age: 30,
+                        country: "Portugal"
+                    },
+                    {
+                        name: "Andrea Ustero Prieto",
+                        age: 18,
+                        country: "Spain"
+                    }
+                ],
+                score: [6, 6]
+            },
+        },
+        {
+            id: "",
+            stage: "Octavos de final",
+            coupleA: {
+                players: [
+                    {
+                        name: "Beatriz González",
+                        age: 28,
+                        country: "Spain"
+                    },
+                    {
+                        name: "Claudia Fernández",
+                        age: 28,
+                        country: "Spain"
+                    }
+                ],
+                score: [6, 6]
+            },
+            coupleB: {
+                players: [
+                    {
+                        name: "Marta Borrero",
+                        age: 22,
+                        country: "Spain"
+                    },
+                    {
+                        name: "Agueda Perez Ortiz",
+                        age: 18,
+                        country: "Spain"
+                    }
+                ],
+                score: [0, 3]
+            },
+        },
+        {
+            id: "",
+            stage: "Octavos de final",
+            coupleA: {
+                players: [
+                    {
+                        name: "Lucía Martínez",
+                        age: 34,
+                        country: "Spain"
+                    },
+                    {
+                        name: "Marta Marrero",
+                        age: 42,
+                        country: "Spain"
+                    }
+                ],
+                score: [3, 5]
+            },
+            coupleB: {
+                players: [
+                    {
+                        name: "Claudia Jensen",
+                        age: 20,
+                        country: "Argentina"
+                    },
+                    {
+                        name: "Alejandra Alonso De Villa",
+                        age: 19,
+                        country: "Spain"
+                    }
+                ],
+                score: [6, 7]
+            },
+        },
+        {
+            id: "",
+            stage: "Octavos de final",
+            coupleA: {
+                players: [
+                    {
+                        name: "Marta Ortega",
+                        age: 28,
+                        country: "Spain"
+                    },
+                    {
+                        name: "Tamara Icardo",
+                        age: 29,
+                        country: "Spain"
+                    }
+                ],
+                score: [6, 6]
+            },
+            coupleB: {
+                players: [
+                    {
+                        name: "Sofía Saiz Vallejo",
+                        age: 22,
+                        country: "Spain"
+                    },
+                    {
+                        name: "Marina Lobo",
+                        age: 22,
+                        country: "Spain"
+                    }
+                ],
+                score: [4, 1]
+            },
+        },
+                {
+            id: "",
+            stage: "Octavos de final",
+            coupleA: {
+                players: [
+                    {
+                        name: "Carmen Goenaga",
+                        age: 21,
+                        country: "Spain"
+                    },
+                    {
+                        name: "Beatriz Caldera Sánchez",
+                        age: 23,
+                        country: "Spain"
+                    }
+                ],
+                score: [0, 3]
+            },
+            coupleB: {
+                players: [
+                    {
+                        name: "Gemma Triay",
+                        age: 33,
+                        country: "Spain"
+                    },
+                    {
+                        name: "Delfina Brea",
+                        age: 25,
+                        country: "Argentina"
+                    }
+                ],
+                score: [6, 6]
+            },
+        }
+        
+    ],
+    quarterfinals: [
+        {
+            id: "",
+            stage: "Cuartos de final",
+            coupleA: {
+                players: [
+                    {
+                        name: "Paula Josemaría",
+                        age: 28,
+                        country: "Spain"
+                    },
+                    {
+                        name: "Ariana Sánchez",
+                        age: 28,
+                        country: "Spain"
+                    }
+                ],
+                score: [6, 6]
+            },
+            coupleB: {
+                players: [
+                    {
+                        name: "Jessica Castello",
+                        age: 26,
+                        country: "Spain"
+                    },
+                    {
+                        name: "Lorena Rufo Ortiz",
+                        age: 22,
+                        country: "Spain"
+                    }
+                ],
+                score: [2, 1]
+            },
+        },
+        {
+            id: "",
+            stage: "Cuartos de final",
+            coupleA: {
+                players: [
+                    {
+                        name: "Marina Guinart España",
+                        age: 29,
+                        country: "Spain"
+                    },
+                    {
+                        name: "Victoria Iglesias",
+                        age: 31,
+                        country: "Spain"
+                    }
+                ],
+                score: [5, 6]
+            },
+            coupleB: {
+                players: [
+                    {
+                        name: "Sofía Arújo",
+                        age: 30,
+                        country: "Portugal"
+                    },
+                    {
+                        name: "Andrea Ustero Prieto",
+                        age: 18,
+                        country: "Spain"
+                    }
+                ],
+                score: [7, 7]
+            },
+        },
+        {
+            id: "",
+            stage: "Cuartos de final",
+            coupleA: {
+                players: [
+                    {
+                        name: "Beatriz González",
+                        age: 28,
+                        country: "Spain"
+                    },
+                    {
+                        name: "Claudia Fernández",
+                        age: 28,
+                        country: "Spain"
+                    }
+                ],
+                score: [6, 7]
+            },
+            coupleB: {
+                players: [
+                    {
+                        name: "Claudia Jensen",
+                        age: 20,
+                        country: "Argentina"
+                    },
+                    {
+                        name: "Alejandra Alonso De Villa",
+                        age: 19,
+                        country: "Spain"
+                    }
+                ],
+                score: [2, 6]
+            },
+        },
+        {
+            id: "",
+            stage: "Cuartos de final",
+            coupleA: {
+                players: [
+                    {
+                        name: "Marta Ortega",
+                        age: 28,
+                        country: "Spain"
+                    },
+                    {
+                        name: "Tamara Icardo",
+                        age: 29,
+                        country: "Spain"
+                    }
+                ],
+                score: [3, 4]
+            },
+            coupleB: {
+                players: [
+                    {
+                        name: "Gemma Triay",
+                        age: 33,
+                        country: "Spain"
+                    },
+                    {
+                        name: "Delfina Brea",
+                        age: 25,
+                        country: "Argentina"
+                    }
+                ],
+                score: [6, 6]
+            },
+        }
+
+    ],
+    semifinals: [
+        {
+            id: "",
+            stage: "Semifinal",
+            coupleA: {
+                players: [
+                    {
+                        name: "Gemma Triay",
+                        age: 33,
+                        country: "Spain"
+                    },
+                    {
+                        name: "Delfina Brea",
+                        age: 25,
+                        country: "Argentina"
+                    }
+                ],
+                score: [7, 3, 7]
+            },
+            coupleB: {
+                players: [
+                    {
+                        name: "Beatriz González",
+                        age: 28,
+                        country: "Spain"
+                    },
+                    {
+                        name: "Claudia Fernández",
+                        age: 28,
+                        country: "Spain"
+                    }
+                ],
+                score: [6, 6, 6]
+            },
+        },
+        {
+            id: "",
+            stage: "Semifinal",
+            coupleA: {
+                players: [
+                    {
+                        name: "Paula Josemaría",
+                        age: 28,
+                        country: "Spain"
+                    },
+                    {
+                        name: "Ariana Sánchez",
+                        age: 28,
+                        country: "Spain"
+                    }
+                ],
+                score: [7, 6]
+            },
+            coupleB: {
+                players: [
+                    {
+                        name: "Sofía Arújo",
+                        age: 30,
+                        country: "Portugal"
+                    },
+                    {
+                        name: "Andrea Ustero Prieto",
+                        age: 18,
+                        country: "Spain"
+                    }
+                ],
+                score: [6, 3]
+            },
+        }
+    ],
+    final: [
+        {
+            id: "",
+            stage: "Finals",
+            coupleA: {
+                players: [
+                    {
+                        name: "Gemma Triay",
+                        age: 33,
+                        country: "Spain"
+                    },
+                    {
+                        name: "Delfina Brea",
+                        age: 25,
+                        country: "Argentina"
+                    }
+                ],
+                score: [7, 6]
+            },
+            coupleB: {
+                players: [
+                    {
+                        name: "Paula Josemaría",
+                        age: 28,
+                        country: "Spain"
+                    },
+                    {
+                        name: "Ariana Sanchez",
+                        age: 28,
+                        country: "Spain"
+                    }
+                ],
+                score: [6, 4]
+            },
+        }
+    ]
+
+}
+
+export default results
